e5b5fd546675ea4d8420a3294f897f11afad8a1b
linuxwt <tengwanginit@gmail.com>
2026-05-26 14:50:44 +0800

    zasdds

--- src/main/java/com/example/user/repository/UserLoginLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.repository;
-
-import com.example.user.domain.UserLoginLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoginLogRepository extends JpaRepository<UserLoginLog, Long> {
-
-}
